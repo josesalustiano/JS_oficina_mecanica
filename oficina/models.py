df60ba1573b3f10a835d2d2c423eba455a13d904
José Salustiano <josesalustianojr@gmail.com>
2026-06-08 23:53:37 -0300

    feat: adicionando model de Marca

--- oficina/models.py
+++ oficina/models.py
@@ -13,3 +13,8 @@ class Cliente(models.Model):
     def __str__(self):
         return self.nome
     
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
